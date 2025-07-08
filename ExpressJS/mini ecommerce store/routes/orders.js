ebde6c10e9ae20bd6b58d02fbdb1cb8d8cd9fa62
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-07-08 14:23:24 -0600

    adds routes to orders and users

--- ExpressJS/mini ecommerce store/routes/orders.js
+++ ExpressJS/mini ecommerce store/routes/orders.js
@@ -1,8 +1,51 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+let userOrders = {
+  1: [{ id: 1, item: "Shoes" }],
+  2: [{ id: 1, item: "Hat" }],
+};
+
+router.use((req, res, next) => {
+  const targetId = parseInt(req.params.userId);
+
+  if (!targetId) {
+    return res.send("Error, that is not a number id");
+  }
+  req.userId = targetId;
+  next();
+});
+
 router.get("/", (req, res, next) => {
-  res.send(`All orders for ${req.params.userId}`);
+  const orders = userOrders[req.userId] || [];
+
+  if (!orders) {
+    return res.status(404).send("No orders found for this user");
+  }
+
+  res.json(orders);
+});
+
+router.post("/", (req, res, next) => {
+  const { item } = req.body;
+
+  if (!item) {
+    return res.status(400).send("Item is required");
+  }
+
+  // If this user has no orders yet, start their array
+  if (!userOrders[req.userId]) {
+    userOrders[req.userId] = [];
+  }
+
+  const newOrder = {
+    id: userOrders[req.userId].length + 1,
+    item,
+  };
+
+  userOrders[req.userId].push(newOrder);
+
+  res.status(201).json(newOrder);
 });
 
 module.exports = router;
--- ExpressJS/mini ecommerce store/routes/users.js
+++ ExpressJS/mini ecommerce store/routes/users.js
@@ -1,8 +1,52 @@
 const express = require("express");
 const router = express.Router();
 
+let users = [
+  { id: 1, name: "abdul", email: "abdul@email.com" },
+  { id: 2, name: "hassan", email: "hassan@email.com" },
+];
+
+router.param("id", (req, res, next, id) => {
+  const user = users.find((u) => u.id === parseInt(id));
+
+  console.log("Router param used!");
+  if (!user) return res.status(404).send("User not found");
+  req.user = user;
+  next();
+});
+
 router.get("/", (req, res, next) => {
-  res.send("all [resource]");
+  res.type("text/plain");
+  res.send(users.map((user) => user.name).join("\n"));
+});
+
+router.get("/:id", (req, res, next) => {
+  res.type("text/plain").send(`User found: ${req.user.name}`);
+});
+
+router.post("/", (req, res, next) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).send("Missing input fields for name or email");
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email,
+  };
+
+  users.push(newUser);
+  res.status(201).send("User successfully added!");
+});
+
+router.delete("/:id", (req, res, next) => {
+  const userIndex = users.findIndex((user) => user.id === req.user.id);
+
+  users.splice(userIndex, 1);
+  res.type("text/plain");
+  res.json({ message: "User deleted", user: req.user });
 });
 
 module.exports = router;
